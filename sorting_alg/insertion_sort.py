3feae022a79c86cd4318b883e30bcacf15769578
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2024-12-29 13:11:46 +0200

    Refactor insertion sort

--- sorting_alg/insertion_sort.py
+++ sorting_alg/insertion_sort.py
@@ -1,9 +1,14 @@
 def insertion_sort(arr:list, arr_len:int):
+    print(f"\nOriginal array: {arr}")
+
     for i in range(1, arr_len):
+
         for j in range(0, i+1):
+            
             if arr[j] >= arr[i]:
                 arr[j], arr[i] = arr[i], arr[j]
-    print(arr)
+    
+    print(f"\nSorted array: {arr}")
 
 
 if __name__ == '__main__':
